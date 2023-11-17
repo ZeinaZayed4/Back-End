9a50ec1a053d1842c2b4114fa71c8fc3b9ff4634
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2023-11-17 11:46:09 +0200

    Q3

---
+++ Exam/Question3.php
@@ -0,0 +1,23 @@
+<?php
+// 1- Dynamic binding is being used for overloaded methods
+// and Static binding is being used for overridden/overriding method.
+// (True)
+
+
+// 2- Abstract Method Cannot Be Inherited.
+// (False)
+
+
+// 3- Abstraction means hiding the internal details or mechanics of how
+// an object does something.
+// (True)
+
+
+// 4- Traits are a mechanism for code reuse in single inheritance
+// languages.
+// (False)
+
+
+// 5- Static functions are associated with the class, not an instance of
+// the class.
+// (True)
